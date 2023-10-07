4fe95a50cdba29e78675a1338a7e39bbc511053c
nimacortez <cortez.nima@gmail.com>
2023-10-07 14:20:09 -0400

    styling signup page

--- src/components/background.jsx
+++ src/components/background.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from 'react';
+import background from '../assets/background.jpg';
+import styled from 'styled-components';
 
-export default function background() {
+export default function BackgroundImage() {
   return (
-    <div>background</div>
-  )
+    <Container>
+      <img src={background} alt="background" />
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+    img {
+      height: 100vh;
+      width: 100vw;
+    } 
+    `;
--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 import styled from 'styled-components';
 import Background from '../components/background';
 import Header from '../components/header';
 
 export default function Signup() {
+  const navigate = useNavigate();
   return (
     <Container>
       <Background />
+      <div className="content">
       <Header />
       <div className="body flex column a-center j-center">
         <div className="text flex column"> 
@@ -21,8 +24,21 @@ export default function Signup() {
         </div>
         <button>Log In</button>
       </div>
+      </div>
     </Container>
   )
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  position: relative; 
+    .content {
+      position: absolute; 
+      top: 0;
+      left: 0;
+      background-color: rgba(0,0,0,0.5);
+      height: 100vh;
+      width: 100vw;
+      display: grid; 
+      grid-template-rows: 15vh 85vh;
+    }
+`;
