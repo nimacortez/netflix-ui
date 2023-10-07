a210f9aaf73cefc5584d056e3448dbe1dbc5cc6b
nimacortez <cortez.nima@gmail.com>
2023-10-07 13:46:46 -0400

    created signup page

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import Background from '../components/background';
+import Header from '../components/header';
 
-export default function signup() {
+export default function Signup() {
   return (
-    <div>signup</div>
+    <Container>
+      <Background />
+      <Header />
+      <div className="body flex column a-center j-center">
+        <div className="text flex column"> 
+          <h1>Unlimited movies, TV shows and more.</h1>
+          <h4>Watch anywhere. Cancel anytime.</h4>
+          <h6>Ready to watch? Enter your email to create your membership.</h6>
+        </div>
+        <div className="form">
+          <input type="email" placeholder="Email Address" name="email" />
+          <input type="password" placeholder="Password" name="password" />
+          <button>Get Started</button>
+        </div>
+        <button>Log In</button>
+      </div>
+    </Container>
   )
 }
+
+const Container = styled.div``;
