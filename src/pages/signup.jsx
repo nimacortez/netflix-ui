aa4e8ee63e3c6def3ec5854227a77f333f482fbc
nimacortez <cortez.nima@gmail.com>
2023-10-09 17:36:09 -0400

    fixed error with the DOM & password prop

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,106 +1,127 @@
 import React, { useState } from 'react';
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import Background from '../components/background';
 import Header from '../components/header';
 
 export default function Signup() {
   const navigate = useNavigate();
-  const [showPassword, setShowPassword] = useState(false); 
+  const [showPassword, setShowPassword] = useState(false);
   const [formValues, setFormValues] = useState({
-    email: "",
-    password: "",
-  })
+    email: '',
+    password: '',
+  });
 
   const handleSignIn = async () => {
     console.log(formValues);
-  }
+  };
+
   return (
-    <Container showPassword={showPassword}>
+    <Container>
       <Background />
       <div className="content">
-      <Header login/>
-      <div className="body flex column a-center">
-        <div className="text flex column a-center" style={{margin: '20px'}}> 
-          <h1>Unlimited movies, TV shows and more.</h1>
-          <h4>Watch anywhere. Cancel anytime.</h4>
-          <h6>Ready to watch? Enter your email to create your membership.</h6>
+        <Header login />
+        <div className="body flex column a-center">
+          <div className="text flex column a-center" style={{ margin: '20px' }}>
+            <h1>Unlimited movies, TV shows and more.</h1>
+            <h4>Watch anywhere. Cancel anytime.</h4>
+            <h6>Ready to watch? Enter your email to create your membership.</h6>
+          </div>
+          <div className="form text flex column a-center">
+            <input
+              type="email"
+              placeholder="Email address"
+              onChange={(e) =>
+                setFormValues({
+                  ...formValues,
+                  [e.target.name]: e.target.value,
+                })
+              }
+              name="email"
+              value={formValues.email}
+            />
+            {showPassword && (
+              <input
+                type="password"
+                placeholder="Password"
+                onChange={(e) =>
+                  setFormValues({
+                    ...formValues,
+                    [e.target.name]: e.target.value,
+                  })
+                }
+                name="password"
+                value={formValues.password}
+              />
+            )}
+            {!showPassword && (
+              <button onClick={() => setShowPassword(true)}>Get Started</button>
+            )}
+          </div>
+          <button onClick={handleSignIn}>Sign Up</button>
         </div>
-        <div className="form text flex column a-center">
-          <input type="email" placeholder="Email Address" name="email" value={formValues.email} onChange={(e) => setFormValues({...formValues,[e.target.name]: e.target.value,})
-        } />
-          {
-            showPassword &&
-          <input type="password" placeholder="Password" name="password" value={formValues.password} onChange={(e) => setFormValues({...formValues,[e.target.name]: e.target.value,})
-        } />}
-          {
-            !showPassword && 
-          <button onClick={() => setShowPassword(true)}>Get Started</button>}
-        </div>
-        <button onClick={handleSignIn}>Sign Up</button>
-      </div>
       </div>
     </Container>
-  )
+  );
 }
 
 const Container = styled.div`
-  position: relative; 
-    .content {
-      position: absolute; 
-      top: 0;
-      left: 0;
-      background-color: rgba(0,0,0,0.5);
-      height: 100vh;
-      width: 100vw;
-      display: grid; 
-      grid-template-rows: 15vh 85vh;
-      .body {
-        gap: 1rem;
-        .text {
-          gap: 1rem; 
-          text-align center; 
-          font-size: 1.5rem;
-          h1 {
-            padding: 0 25 rem; 
-          }
-        }
-        .form {
-          display: grid;
-          grid-template-columns: ${({ showPassword }) =>
-            showPassword ? "1fr 1fr" : "2fr 1fr"};
-          width: 60%;
-          input {
-            color: black;
-            border: none;
-            padding: 1.5rem;
-            font-size: 1.2rem;
-            border: 1px solid black;
-            &:focus {
-              outline: none;
-            }
-          }
-          button {
-            padding: 1.5rem 1.5rem;
-            background-color: #e50914;
-            border: none;
-            cursor: pointer;
-            color: white;
-            font-weight: bolder;
-            font-size: 1.35rem;
-            border-radius: 0.55rem;
-            }
-          }
-          button {
-            padding: 0.5rem 1.5rem;
-            background-color: #e50914;
-            border: none;
-            cursor: pointer;
-            color: white;
-            border-radius: 0.3rem;
-            font-weight: bolder;
-            font-size: 1.35rem;
-          }
+  position: relative;
+  .content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    height: 100vh;
+    width: 100vw;
+    display: grid;
+    grid-template-rows: 15vh 85vh;
+  }
+  .body {
+    gap: 1rem;
+  }
+  .text {
+    gap: 1rem;
+    text-align: center;
+    font-size: 1.5rem;
+    h1 {
+      padding: 0 25rem;
+    }
+  }
+  .form {
+    display: grid;
+    grid-template-columns: ${({ showPassword }) =>
+      showPassword ? '1fr 1fr' : '2fr 1fr'};
+    width: 60%;
+    input {
+      color: black;
+      border: none;
+      padding: 1.5rem;
+      font-size: 1.2rem;
+      border: 1px solid black;
+      &:focus {
+        outline: none;
       }
     }
+    button {
+      padding: 1.5rem 1.5rem;
+      background-color: #e50914;
+      border: none;
+      cursor: pointer;
+      color: white;
+      font-weight: bolder;
+      font-size: 1.35rem;
+      border-radius: 0.55rem;
+    }
+  }
+  button {
+    padding: 0.5rem 1.5rem;
+    background-color: #e50914;
+    border: none;
+    cursor: pointer;
+    color: white;
+    border-radius: 0.3rem;
+    font-weight: bolder;
+    font-size: 1.35rem;
+  }
 `;
