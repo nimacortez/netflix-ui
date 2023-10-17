decba5e4e7f5125ce852e97ed7da5a273e9758dd
nimacortez <cortez.nima@gmail.com>
2023-10-17 19:23:11 -0400

    fixed login shadow

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -35,6 +35,7 @@ export default function Login() {
         <Header />
         <div className="form-container flex column a-center j-center-">
           <div className="form flex column a-center j-center">
+            <div className="title">
             <h3>Login</h3>
           </div>
           <div className='container flex column'>
@@ -63,6 +64,7 @@ export default function Login() {
                 value={formValues.password}
               />
               <button onClick={handleLogIn}>Login</button>
+            </div>  
           </div>
         </div>
       </div>
@@ -72,44 +74,43 @@ export default function Login() {
 
 
 const Container = styled.div`
-position: relative;
-.content {
-  position: absolute;
-  top: 0;
-  left: 0;
-  background-color: rgba(0, 0, 0, 0.5);
-  height: 100vh;
-  width: 100vw;
-  display: grid;
-  grid-template-rows: 15vh 85vh;
-  .form-container {
-    gap: 2rem;
-    height: 85vh;
-    .form {
-      padding: 2rem; 
-      background-color: #000000b0; 
-      width: 25vw; 
-      gap: 2rem; 
-      color: white;
-      .container {
-        gap: 2rem; 
-        input {
-          padding: 0.5rem 1rem; 
-          width: 15rem;
-        }
-        button {
-          padding: 0.5rem 1.5rem;
-          background-color: #e50914;
-          border: none;
-          cursor: pointer;
-          color: white;
-          border-radius: 0.3rem;
-          font-weight: bolder;
-          font-size: 1.35rem;
+  position: relative;
+  .content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 100vw;
+    background-color: rgba(0, 0, 0, 0.5);
+    grid-template-rows: 15vh 85vh;
+    .form-container {
+      gap: 2rem;
+      height: 85vh;
+      .form {
+        padding: 2rem;
+        background-color: #000000b0;
+        width: 25vw;
+        gap: 2rem;
+        color: white;
+        .container {
+          gap: 2rem;
+          input {
+            padding: 0.5rem 1rem;
+            width: 15rem;
+          }
+          button {
+            padding: 0.5rem 1rem;
+            background-color: #e50914;
+            border: none;
+            cursor: pointer;
+            color: white;
+            border-radius: 0.2rem;
+            font-weight: bolder;
+            font-size: 1.05rem;
+          }
         }
       }
     }
   }
-}
-
 `;
+
